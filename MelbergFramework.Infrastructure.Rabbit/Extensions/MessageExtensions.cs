50e5aa657c9a122a2f1f2cf1ad280848df63d882
Joseph-Melberg <joseph.melberg@myqops.com>
2024-01-16 20:04:44 -0600

    Fixing memory leaks

--- MelbergFramework.Infrastructure.Rabbit/Extensions/MessageExtensions.cs
+++ MelbergFramework.Infrastructure.Rabbit/Extensions/MessageExtensions.cs
@@ -7,18 +7,29 @@ internal static class MessageExtensions
 {
     public static DateTime GetTimestamp(this Message message )
     {
-        if(message.Headers.TryGetValue(Headers.Timestamp,out var timestamp))
+        if(message.Headers.TryGetValue(Headers.Timestamp,out var timestamp) && timestamp is string)
         {
             try
             {
                 
                 return DateTime.ParseExact((string)timestamp,"o", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal); 
             }
-            catch (System.Exception)
-            {
-                
-            }
+            catch (Exception) { }
         }
         return DateTime.UtcNow;
+    }
+    
+    public static Guid GetCoID(this Message message)
+    {
+        if(message.Headers.TryGetValue(Headers.CorrelationId, out var coid) && coid is string)
+        {
+            try
+            {
+                return Guid.Parse((string)coid);
+            }
+            catch(Exception){}
+        }
+        
+        return Guid.NewGuid();
     }
 }
